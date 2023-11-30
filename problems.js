2f98aa84741ddf49d09fb34bb1601f74ac7a0f5c
Kevin <kevindbaik@gmail.com>
2023-11-29 16:13:44 -0800

    longest-common-prefix

--- problems.js
+++ problems.js
@@ -36,3 +36,17 @@ var isPalindrome = function(x) {
 };
 
 // https://leetcode.com/problems/longest-common-prefix/
+
+var longestCommonPrefix = function(strs) {
+  let answer = '';
+
+  if(strs.length === 0) return answer;
+
+  for (let i = 0; i < strs[0].length; i++) {
+      for (let j = 1; j < strs.length; j++) {
+          if (strs[j][i] !== strs[0][i]) return answer
+      }
+      answer += strs[0][i]
+  }
+  return answer
+};
